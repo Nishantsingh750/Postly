b814201254a212d0b45636cdfa8182d1982536ca
[firstname <nishantsingh7508@gmail.com>
2025-05-17 16:54:43 +0530

    update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import {Container} from '../components'
+import {PostCard} from '../components/PostCard'
 
 function Home() {
     const [posts, setPosts] = useState([])
